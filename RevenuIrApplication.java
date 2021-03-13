f21f71b4af5f061e7f7228897270c6873f6043c3
mohamedouakib <79708447+mohamedouakib@users.noreply.github.com>
2021-03-13 19:55:25 +0100

    Add files via upload

---
+++ RevenuIrApplication.java
@@ -0,0 +1,13 @@
+package com.impot.revenuir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenuIrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevenuIrApplication.class, args);
+    }
+
+}
